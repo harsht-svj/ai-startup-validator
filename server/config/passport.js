f2a332f429ebb31496912762132657d55b1cefbe
harshit <munjalharshit0@gmail.com>
2026-07-03 14:08:55 +0530

    Fix last case-sensitive import in passport.js

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,6 +1,6 @@
 import passport from 'passport'
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
-import User from '../models/User.js'
+import User from '../models/user.js'
 
 // JWT Strategy only
 passport.use(new JwtStrategy(
